3e7a30c0c9657f0f0f53e352473afce9d2038d06
ryo-ha <80822677+ryo-ha-gi@users.noreply.github.com>
2024-03-09 16:55:30 +0000

    Refactor: コンポーネントファイルを移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Grid from "./grid"
+import Grid from "../components/grid"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
